dd91d60b055006ae243548309c72d46aa633f7b7
Robson Adorno <rferreira@scopus.com.br>
2019-06-26 16:14:08 -0300

    DASDSAD

---
+++ TESTEAPPCENTER/TESTEAPPCENTER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TESTEAPPCENTER
+//
+//  Created by Robson Adorno on 26/06/19.
+//  Copyright © 2019 Mrs Fantasy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+     func testBla(bla:String) -> String?{
+        if bla != nil{
+            return bla
+        }
+            return nil
+        }
+}
+
